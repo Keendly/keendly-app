da031bb0342f671a351f17b4b925babbcc09700e
radek <moomeen@gmail.com>
2018-01-15 23:59:25 +0100

    icons

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 self.addEventListener ('push', function (event) {
   console.log ('[Service Worker] Push Received.');
-  console.log ('[Service Worker] Push had this data: "${event.data.text ()}"');
+  console.log (event.data.text ());
 
-  const title = 'Push from firebase';
+  const title = 'Articles delivered';
   const options = {
-    body: '', //Your push message - event.data.text() for instance
-    icon: '', //image
-    badge: '', ////image
+    body: event.data.text (),
+    icon: 'https://app.keendly.com/images/icons/icon-192x192.png',
+    badge: 'https://app.keendly.com/images/icons/icon-128x128.png',
   };
 
   event.waitUntil (self.registration.showNotification (title, options));
